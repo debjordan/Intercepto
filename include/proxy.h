b33908f7164efebd6403da3167237756eb8b489a
jordantargz <joaojordan.shell@gmail.com>
2024-12-01 16:25:21 -0300

    feat: configurando o proxy http

---
+++ include/proxy.h
@@ -0,0 +1,15 @@
+#ifndef PROXY_H
+#define PROXY_H
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <string>
+
+// configuracao do proxy http
+class Proxy {
+public:
+    void start(const std::string& host, unsigned short port);
+private:
+    void handle_request(boost::asio::ip::tcp::socket& socket);
+};
+#endif
